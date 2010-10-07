98d727289d974d133e35ec1a8dcfa942f578d471
Livingston Samuel <livingston.samuel@gmail.com>
2010-10-07 16:34:16 +0530

    Added gc - git clone script

---
+++ gc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+#Short command to clone a git repository
+## USAGE: gc git://github.com/livingston/rotator.git
+## this command will clone the git repo in a folder of the format {GITHUB_USER-NAME}_{REPO-NAME}
+
+gc()
+{
+  str=$1
+
+  uvar=`echo $str | sed s:\.git::g | awk -F '/' '{printf("%s_%s", $3, $4)}'`
+
+  git clone $str $uvar
+}
