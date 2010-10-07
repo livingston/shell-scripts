f80bc522526ca28945e1e3ce7a760f39dc0a4a7d
Livingston Samuel <livingston.samuel@gmail.com>
2010-10-07 17:35:06 +0530

    Updated gua - suppressed output, added progress

--- gua.sh
+++ gua.sh
@@ -5,13 +5,19 @@
 gua()
 {
 base=$(pwd)
-for folder in $(find -type d -maxdepth 1)
+folder_list=$(find -type d -maxdepth 1)
+folder_size=$(find -type d -maxdepth 1| wc -l)
+count=0
+for folder in $folder_list
 do
+  count=$[$count+1]
+  echo -ne "Updating $count of $folder_size \r";
+
   cd $folder
   if [ -d ".git" ]; then
-    echo "UPDATING $folder ..."
-    git pull
+    git pull > /dev/null 2>&1
   fi
   cd $base
 done
+echo -ne '\n'
 }
